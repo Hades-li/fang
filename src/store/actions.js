06af7387649a6e402971e9fdac10bd7ff090f86b
plusli <121318100@qq.com>
2018-03-03 12:55:18 +0800

    更新

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,8 +3,8 @@ import * as types from './mutation-types'
 import state from "./index"
 import axios from "axios"
 import qs from "qs"
-import router from "router" 
-import iView from "iView"
+import router from "router"
+// import iView from "iView"
 
 export const actions = {
     // 登陆
@@ -12,7 +12,7 @@ export const actions = {
         axios.post(
             state.state.MainUrl + '/index?opt=100',
             qs.stringify(
-                { 
+                {
                     'mobile': 15913351558,
                     "password":234567,
                     "smsCode":'',
@@ -33,7 +33,7 @@ export const actions = {
         axios.post(
             state.state.MainUrl + '/index?opt=103',
             qs.stringify(
-                { 
+                {
                     'mobile':data.user,
                     "password":data.password1,
                     "imgCode":'',
@@ -44,11 +44,11 @@ export const actions = {
             if(response.data.success == false){
                 iView.Message.error({
                     content:response.data.msg
-                })    
+                })
             }else if(response.data.success == true){
                 iView.Message.success({
                     content:response.data.msg
-                })  
+                })
             }
         }).catch(function (error) {
                 console.log(error);
@@ -59,7 +59,7 @@ export const actions = {
         axios.post(
             state.state.MainUrl + '/index?opt=101',
             qs.stringify(
-                { 
+                {
                     'mobile':data.mobile,
                     "sms_type":data.sms_type,
                 }
@@ -67,7 +67,7 @@ export const actions = {
                 if(response.data.success == false){
                     iView.Message.error({
                         content:response.data.msg
-                    })    
+                    })
                 }else if(response.data.success == true){
 
                 }
@@ -76,7 +76,7 @@ export const actions = {
                 console.log(error);
         });
     },
-    
+
     changeTabBar(context,tabBar){
         context.commit(types.SET_TABBAR,{
             tabBar:tabBar
